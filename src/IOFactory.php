b8b2e1f7ca66cb4fe8a0e2970ff039f6eac527af
Marko Harjula <marko.harjula@gmail.com>
2016-04-08 13:01:48 +0300

    Create IOFactory.php

---
+++ src/IOFactory.php
@@ -0,0 +1,15 @@
+<?php
+namespace mharj;
+/**
+ * abstract IO Factory for generic IO conversions
+ */
+abstract class IOFactory {
+	/**
+	 * string to object conversion
+	 */
+	abstract function fromString($string);
+	/**
+	 * object to string conversion
+	 */ 
+	abstract function toString($object);
+}
