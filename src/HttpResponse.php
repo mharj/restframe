c05e4068f127b87224a3a1a0fb643fd7a8c6c067
Marko Harjula <marko.harjula@gmail.com>
2016-05-16 11:28:42 +0300

    Update HttpResponse.php

--- src/HttpResponse.php
+++ src/HttpResponse.php
@@ -2,5 +2,17 @@
 namespace mharj;
 
 class HttpResponse {
-
+  private $headers = array();
+  public function addHeader(string $name, string $value) {
+    $this->headers[$name]=$value;
+  }
+  public function getHeaderNames(): array {
+    return array_keys($headers);
+  }
+  public function getHeader(string $name) {
+    if ( isset($this->headers[$name]) ) {
+      return $this->headers[$name];
+    }
+    return null;
+  }
 }
