6fd2b565a4db28b838d3f9ba45d1658d168c0f28
Marko Harjula <marko.harjula@gmail.com>
2016-05-16 13:24:20 +0300

    Update HttpResponse.php

--- src/HttpResponse.php
+++ src/HttpResponse.php
@@ -3,15 +3,29 @@ namespace mharj;
 
 class HttpResponse {
   private $headers = array();
+  
+  /**
+   *  Add value end of array or create new one if not existing
+   */
   public function addHeader(string $name, string $value) {
-    $this->headers[$name]=$value;
+    if (! isset($this->headers[$name]) ) {
+      $this->headers[$name]=array($value);
+    } else {
+      $this->headers[$name][]=$value;
+    }
   }
+  
+  /**
+   * Set value and override all existing ones in array
+   */
   public function setHeader(string $name, string $value) {
-    $this->headers[$name]=$value;
+    $this->headers[$name]=array($value);
   }  
+  
   public function getHeaderNames(): array {
     return array_keys($this->headers);
   }
+  
   public function getHeader(string $name) {
     if ( isset($this->headers[$name]) ) {
       return $this->headers[$name];
