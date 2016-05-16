f521b3c7ea5a01b004961c404ab24ec97d9f3329
Marko Harjula <marko.harjula@gmail.com>
2016-05-16 12:17:05 +0300

    Update HttpRequest.php

--- src/HttpRequest.php
+++ src/HttpRequest.php
@@ -1,6 +1,30 @@
 <?php
 namespace mharj;
 
-class HttpRequest {
-
+abstract class HttpRequest {
+  private $headers = array();
+  public function __construct() {
+    $this->headers = getallheaders();
+  }
+  
+  public function getHeaderNames(): array {
+    return array_keys($this->headers);
+  }
+  
+  public function getHeader(string $name) {
+    if ( isset($this->headers[$name]) ) {
+      return $this->headers[$name];
+    }
+    return null;
+  }
+  
+  public static function getServerHttpRequest() {
+    if ( defined('HHVM_VERSION') ) {
+      return new HhvmHttpRequest();
+    }
+    if( strpos( $_SERVER['SERVER_SOFTWARE'], 'Apache') !== false) {
+      return new ApacheHttpRequest();
+    } 
+    return new CommonHttpRequest();
+  }
 }
