546ea156c0bd4ec561d0ad573670e18ef5affc37
Marko Harjula <marko.harjula@gmail.com>
2016-05-19 12:07:09 +0300

    Update RestFrame.php
    Test header change

--- src/RestFrame.php
+++ src/RestFrame.php
@@ -37,12 +37,13 @@ abstract class RestFrame {
 		http_response_code( $this->resp->getStatus() );
 	}
 	private function write($data) {
-		$this->buildHeaders();
 		if ( self::$compress == true && substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip') ) {
 			ob_start("ob_gzhandler");
+			$this->buildHeaders();
 			echo $data;
 			ob_flush();
 		} else {
+			$this->buildHeaders();
 			echo $data;
 		}
         }
