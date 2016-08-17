0a1c5d02480b6424f5f578ad272dc5088b8aa7d0
Marko Harjula <marko.harjula@gmail.com>
2016-08-17 14:45:47 +0300

    Cleanup + initial raw mode support

--- src/HttpResponse.php
+++ src/HttpResponse.php
@@ -2,41 +2,50 @@
 namespace mharj;
 
 class HttpResponse {
-  private $headers = array();
-  private $status = 200;
+	private $headers = array();
+	private $status = 200;
+	private $is_raw = false;  
   
-  /**
-   *  Add value end of array or create new one if not existing
-   */
-  public function addHeader(string $name, string $value) {
-    if (! isset($this->headers[$name]) ) {
-      $this->headers[$name]=array($value);
-    } else {
-      $this->headers[$name][]=$value;
-    }
-  }
+	/**
+	 *  Add value end of array or create new one if not existing
+	 */
+	public function addHeader(string $name, string $value) {
+		if (! isset($this->headers[$name]) ) {
+			$this->headers[$name]=array($value);
+		} else {
+			$this->headers[$name][]=$value;
+		}
+	}
   
-  /**
-   * Set value and override all existing ones in array
-   */
-  public function setHeader(string $name, string $value) {
-    $this->headers[$name]=array($value);
-  }  
+	/**
+	 * Set value and override all existing ones in array
+	 */
+	public function setHeader(string $name, string $value) {
+		$this->headers[$name]=array($value);
+	}
   
-  public function getHeaderNames(): array {
-    return array_keys($this->headers);
-  }
+	public function getHeaderNames(): array {
+		return array_keys($this->headers);
+	}
   
-  public function getHeader(string $name) {
-    if ( isset($this->headers[$name]) ) {
-      return $this->headers[$name];
-    }
-    return null;
-  }
-  public function setStatus(int $status) {
-    $this->status = $status;
-  }
-  public function getStatus(): int {
-    return $this->status;
-  }
+	public function getHeader(string $name) {
+		if ( isset($this->headers[$name]) ) {
+			return $this->headers[$name];
+		}
+		return null;
+	}
+	public function setStatus(int $status) {
+		$this->status = $status;
+	}
+	public function getStatus(): int {
+		return $this->status;
+	}
+	
+	public function setRaw(bool $raw) {
+		$this->is_raw = $raw;
+	}
+	
+	public function isRaw(): bool {
+		return $this->is_raw;
+	}	
 }
